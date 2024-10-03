5ea13e6d764e56709b0e51d78629b78599f59923
Naveen Kumar <naveenkumarerroju@gmail.com>
2024-10-03 18:07:01 +0530

    Field validation exceptions are handled properly

--- src/main/java/com/naveen/rentread/controller/ManageBooksController.java
+++ src/main/java/com/naveen/rentread/controller/ManageBooksController.java
@@ -3,19 +3,22 @@ package com.naveen.rentread.controller;
 import com.naveen.rentread.domain.Book;
 import com.naveen.rentread.model.BookCreationRequest;
 import com.naveen.rentread.service.BookService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/manage/book")
+@Validated
 public class ManageBooksController {
 
     @Autowired
     BookService service;
 
     @PostMapping()
-    public ResponseEntity<Book> createBook(@RequestBody BookCreationRequest creationRequest){
+    public ResponseEntity<Book> createBook(@Valid @RequestBody BookCreationRequest creationRequest){
         return ResponseEntity.ok(service.createBook(creationRequest));
     }
 
